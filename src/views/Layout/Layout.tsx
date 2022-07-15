e6de4c5ef053e56d9acdf65e0b64623111f0619b
designbyblake <dblake@webdevelopmentgroup.com>
2022-07-15 18:35:58 -0400

    fix: Path fix

--- src/views/Layout/Layout.tsx
+++ src/views/Layout/Layout.tsx
@@ -1,6 +1,6 @@
-import { Header } from 'views/Layout/components/Header';
 import { Navigation } from 'views/Layout/components/Navigation/Navigation';
 import { Outlet } from 'react-router-dom';
+import { Header } from 'views/Layout/components/Header';
 
 export const Layout = () => (
   <>
